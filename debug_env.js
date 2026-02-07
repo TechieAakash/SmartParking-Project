296dc1001fc4c3e39855143f1cec067b2ee2512a
Aakash Yadav <aakash411883@gmail.com>
2026-02-08 04:32:37 +0530

    Cleanup: Remove legacy root files and redundant scripts

--- debug_env.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('MYSQLHOST:', process.env.MYSQLHOST);
-console.log('MYSQL_URL:', process.env.MYSQL_URL);
-console.log('DB_NAME:', process.env.DB_NAME);
